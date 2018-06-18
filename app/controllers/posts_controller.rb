42e902e8404fccc84a4333243291b7d0b12c2554
Denise Pendleton <dpendl1@hotmail.com>
2018-06-18 16:00:07 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,7 +12,7 @@ class PostsController < ApplicationController
 	end
 
 	def create
-		raise params.inspect
+		# raise params.inspect
 	  @post = Post.new(params[:post])
 	  @post.save
 	  redirect_to post_path(@post)
